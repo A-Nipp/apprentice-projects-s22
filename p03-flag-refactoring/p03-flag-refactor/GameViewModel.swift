964086b75f1abdd51c15244afcf7d0c47281f7ee
A-Nipp <65468669+A-Nipp@users.noreply.github.com>
2022-01-22 16:03:26 -0500

    refactored code according to MVVM

--- p03-flag-refactoring/p03-flag-refactor/GameViewModel.swift
+++ p03-flag-refactoring/p03-flag-refactor/GameViewModel.swift
@@ -6,5 +6,32 @@
 //
 
 import Foundation
+import SwiftUI
 
-#warning("Add GameViewModel class in this file")
+class GameViewModel: ObservableObject {
+    @Published var countries = CountryList.countries.shuffled().prefix(3)
+    @Published var correctAnswerIndex = Int.random(in: 0...2)
+    @Published var score = 0
+    @Published var showingAlert = false
+    @Published var alertTitle = ""
+    var targetCountry: Country {
+        return countries[correctAnswerIndex]
+    }
+    
+    func refreshGame() -> Void {
+        countries = CountryList.countries.shuffled().prefix(3)
+        correctAnswerIndex = Int.random(in: 0...2)
+    }
+    
+    func guessFlag(country: Country) -> Void {
+        if country == targetCountry {
+            alertTitle = "Correct"
+            score += 1
+        } else {
+            alertTitle = "Wrong! Thats the flag of \(country.name)"
+        }
+        
+        showingAlert = true
+    }
+
+}
