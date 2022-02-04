18bd8c0e7846680652bfee82f52252e358691710
A-Nipp <65468669+A-Nipp@users.noreply.github.com>
2022-02-03 20:39:48 -0500

    working like button

--- p05-socialmedia/p05-socialmedia/Post/PostView.swift
+++ p05-socialmedia/p05-socialmedia/Post/PostView.swift
@@ -9,7 +9,12 @@ import SwiftUI
 
 struct PostView: View {
     let post: Post
-    
+    let bodyFontSize = 14.0
+    let vertSpace = 8.0
+    @State private var isLiked = false
+    @State private var likeColor = Color(UIColor.label)
+    @State private var heartIcon = "heart"
+    @State private var heartIconColor = Color(UIColor.systemBlue)
     var body: some View {
         HStack(alignment: .top) {
             VStack {
@@ -20,17 +25,17 @@ struct PostView: View {
                     .clipShape(Circle())
                 //                Spacer()
             }
-            VStack(alignment: .leading) {
+            VStack(alignment: .leading, spacing: vertSpace) {
                 HStack {
-                    VStack(alignment: .leading) {
-                        Text(post.authorName)
-                            .fontWeight(.bold)
-                        Text("@" + post.authorUsername)
-                            .foregroundColor(.gray)
-                    }
-                    
+                    Text(post.authorName)
+                        .fontWeight(.bold)
+                    Text("@" + post.authorUsername)
+                        .foregroundColor(.gray)
                 }
+                
+                
                 Text(post.postContent)
+                    .font(.system(size: bodyFontSize))
                 Text(post.formattedDate)
                     .foregroundColor(.gray)
                     .fontWeight(.light)
@@ -38,21 +43,43 @@ struct PostView: View {
                     Button {} label: {
                         Image(systemName: "bubble.left")
                     }
-                    Button {} label: {
-                        Image(systemName: "arrow.rectanglepath")
+                    Text("\(post.commentCount)")
+//                    Button {} label: {
+//                        Image(systemName: "arrow.rectanglepath")
+//                    }
+                    Button {
+                        withAnimation {
+                            toggleLiked()
+                        }
+                    } label: {
+                        Image(systemName: heartIcon)
+                            .foregroundColor(heartIconColor)
                     }
-                    Button {} label: {
-                        Image(systemName: "heart")
-                    }
+                    Text("\(post.likeCount)")
+                        .foregroundColor(likeColor)
                     
                 }
             }
             //            .padding()
         }
-        .padding()
+        .padding(5)
+        
+    }
+    
+    func toggleLiked() {
+        if isLiked {
+            likeColor = Color(UIColor.label)
+            heartIcon = "heart"
+            heartIconColor = Color(UIColor.systemBlue)
+        }
+        else {
+            likeColor = Color.red
+            heartIcon = "heart.fill"
+            heartIconColor = Color.red
+        }
+        isLiked.toggle()
     }
 }
-
 struct PostView_Previews: PreviewProvider {
     static var previews: some View {
         PostView(post: .example)
