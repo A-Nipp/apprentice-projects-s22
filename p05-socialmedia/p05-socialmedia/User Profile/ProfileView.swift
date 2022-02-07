1cc964d33c9199b28417c9dc83ac8b882a4ba97a
A-Nipp <65468669+A-Nipp@users.noreply.github.com>
2022-02-07 00:51:53 -0500

    created profile view

--- p05-socialmedia/p05-socialmedia/User Profile/ProfileView.swift
+++ p05-socialmedia/p05-socialmedia/User Profile/ProfileView.swift
@@ -13,9 +13,42 @@ struct ProfileView: View {
     let profileImageAddress: String = "appteam" // will eventually be url
     
     let userPosts: [Post] = PostList.defaultPosts
+    let banner = Color.blue
 
     var body: some View {
-        Text("Hello, World!")
+        ScrollView {
+            VStack(alignment: .leading) {
+                Rectangle()
+                    .frame(height:80)
+                    .opacity(0)
+                HStack {
+                    Image(profileImageAddress)
+                        .resizable()
+                        .frame(width: 70.0, height: 70.0)
+                        .clipShape(Circle())
+                    Spacer()
+                    Button {} label: {
+                        Text("Edit Profile")
+                    }
+                }
+                Text(name)
+                    .font(.largeTitle)
+                Text("@\(username)")
+                    .foregroundColor(Color.gray)
+                    .font(.system(size: 14.0))
+                Text("Tweets")
+                    .padding(.vertical)
+                Divider()
+                VStack(alignment:.leading) {
+                    ForEach(userPosts, id: \.id) { post in
+                        PostView(post: post)
+                            .padding(.vertical)
+                        Divider()
+                    }
+                }
+            }
+            .padding()
+        }
     }
 }
 
