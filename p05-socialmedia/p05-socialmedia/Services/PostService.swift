8236fd9f9d345d27a55232ad78d8f441a726206e
A-Nipp <65468669+A-Nipp@users.noreply.github.com>
2022-02-21 02:01:05 -0500

    made post service

---
+++ p05-socialmedia/p05-socialmedia/Services/PostService.swift
@@ -0,0 +1,20 @@
+//
+//  PostService.swift
+//  p05-socialmedia
+//
+//  Created by AlecNipp on 2/21/22.
+//
+
+import Foundation
+
+struct PostService {
+    private static var posts: [Post] = []
+    
+    static func getHomeFeed() -> [Post] {
+        return posts
+    }
+    
+    static func makePost(post: Post) -> Void {
+        posts.insert(post, at: 0)
+    }
+}
